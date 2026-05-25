6a0243bc5dd85b30cbfbe6098e476fe5e5f24e90
SpaceShaman <spaceshaman@tuta.io>
2026-05-25 13:01:01 +0200

    ♻️ (main.py): refactor Bitwarden class to use email instead of client_id and client_secret ✨ (main.py): add Status dataclass and methods for setup, login, logout, and server URL configuration

--- src/bitwarden_py/main.py
+++ src/bitwarden_py/main.py
@@ -1,45 +1,59 @@
+import json
+from dataclasses import dataclass
+from typing import Literal
+
 from .command_runner import run_command
+
+
+@dataclass
+class Status:
+    status: Literal["locked", "unlocked", "unauthenticated"]
+    server_url: str
 
 
 class Bitwarden:
     def __init__(
         self,
-        client_id: str,
-        client_secret: str,
+        email: str,
         password: str,
         server_url: str = "https://vault.bitwarden.eu",
     ):
-        self._client_id = client_id
-        self._client_secret = client_secret
+        self._email = email
         self._password = password
         self._server_url = server_url
+        self._setup()
 
-    def login(self) -> None:
+    def _setup(self) -> None:
+        status = self._get_status()
+        if status.server_url != self._server_url:
+            if status.status != "unauthenticated":
+                self._logout()
+            self._set_server_url()
+        status = self._get_status()
+        if status.status == "unauthenticated":
+            self._login()
+
+    def _get_status(self) -> Status:
+        output = run_command(["bw", "status"])
+        status_data = json.loads(output)
+        return Status(
+            status=status_data.get("status", "unauthenticated"),
+            server_url=status_data.get("serverUrl", ""),
+        )
+
+    def _login(self) -> None:
         run_command(
             [
                 "bw",
                 "login",
-                self._client_id,
-                self._client_secret,
+                self._email,
+                self._password,
                 "--raw",
             ],
         )
 
+    def _logout(self) -> None:
+        run_command(["bw", "logout"])
 
-if __name__ == "__main__":
-    from pathlib import Path
-
-    from dotenv import load_dotenv
-
-    config = load_dotenv(
-        Path(__file__).parent.parent.parent / "secrets" / "bitwarden.env"
-    )
-
-    bitwarden = Bitwarden(
-        client_id=config.get("BW_CLIENT_ID"),  # type: ignore
-        client_secret=config.get("BW_CLIENT_SECRET"),  # type: ignore
-        password=config.get("BW_PASSWORD"),  # type: ignore
-        server_url=config.get("BW_SERVER_URL"),  # type: ignore
-    )
-
-    bitwarden.login()
+    def _set_server_url(self) -> None:
+        run_command(["bw", "config", "server", self._server_url])
